554fd267347b453c79e9c24cca05cd8baf12f1ee
rutaks <samrutakayile@awesomity.rw>
2019-10-14 11:42:11 +0200

    Refactored Structure For VS Code

--- src/codz/assignment/Main.java
+++ src/codz/assignment/Main.java
@@ -1,8 +1,7 @@
-package com.cog;
+package codz.assignment;
 
 public class Main {
-
     public static void main(String[] args) {
-        System.out.println("Initial Setup");
+        System.out.println("Hey");
     }
 }
