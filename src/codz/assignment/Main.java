07d136b56c23c7934c4bf027e7208d05cf7920d9
rutaks <samrutakayile@awesomity.rw>
2019-10-23 16:57:02 +0200

    Implemented Serialization to txt file

--- src/codz/assignment/Main.java
+++ src/codz/assignment/Main.java
@@ -12,11 +12,21 @@ import java.io.ObjectOutputStream;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Pizza[] pizzas = new Pizza[3];
+
         Pizza pizza = new SimplePizza(ESize.S);
 
         pizza = new Pepperoni(pizza);
         pizza = new Beef(pizza);
         System.out.println(pizza);
+
+
+
+        pizzas[0] = pizza;
+
+        ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("A.txt"));
+        out.writeObject(pizza);
+        out.close();
     }
 }
