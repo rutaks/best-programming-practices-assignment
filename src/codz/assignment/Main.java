5c039aaf77937db945c7340fcb2f231eb79256e9
rutaks <samrutakayile@awesomity.rw>
2019-10-23 16:56:20 +0200

    Perform Implementation in Main Method

--- src/codz/assignment/Main.java
+++ src/codz/assignment/Main.java
@@ -1,7 +1,22 @@
 package codz.assignment;
 
+import codz.assignment.decorators.Beef;
+import codz.assignment.decorators.Pepperoni;
+import codz.assignment.decorators.ToppingsDecorator;
+import codz.assignment.enums.ESize;
+import codz.assignment.models.Pizza;
+import codz.assignment.models.SimplePizza;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hey");
+        Pizza pizza = new SimplePizza(ESize.S);
+
+        pizza = new Pepperoni(pizza);
+        pizza = new Beef(pizza);
+        System.out.println(pizza);
     }
 }
